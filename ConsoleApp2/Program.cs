899a6b161ad11251fc39a9f3723d5e8918b468cf
John Ward <johndward01@gmail.com>
2022-09-07 13:43:55 -0500

    first commit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            static void PrintStuff()
+            {
+                Console.WriteLine("Stuff");
+            }
+
+            Console.WriteLine("Hello World!");
+            PrintStuff();
+        }
+
+        
+
+    }
+}
